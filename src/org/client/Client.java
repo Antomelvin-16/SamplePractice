6d6ea9b686c98c076210ae71c41e3a18d8b0e9a2
Antomelvin-16 <antomelvin.benitto@gmail.com>
2026-02-19 12:35:55 +0530

    Melvin done his codings

--- src/org/client/Client.java
+++ src/org/client/Client.java
@@ -6,5 +6,11 @@ public class Client {
     public void clientName() {
         System.out.println("Client Name: ABC Technologies");
     }
+    
+    private void melvinCodings() {
+		
+    	System.out.println("Melvin");
+
+	}
 }
 
