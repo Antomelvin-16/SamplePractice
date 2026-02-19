c9e662e26f87c7af0fae92d18d91c453009c9873
Antomelvin-16 <antomelvin.benitto@gmail.com>
2026-02-19 12:44:52 +0530

    Bala done his codings

--- src/org/client/Client.java
+++ src/org/client/Client.java
@@ -6,5 +6,11 @@ public class Client {
     public void clientName() {
         System.out.println("Client Name: ABC Technologies");
     }
+    
+    private void balaCodings() {
+    	
+    	System.out.println("Bala");
+
+	}
 }
 
